7c511a47b64f5a599b2af79fbbfa3e0352c48fef
Руслан Хакимянов <m_b_t_r@hotmail.com>
2019-12-27 21:23:14 +0300

    save

--- verygood/urls.py
+++ verygood/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from blog import views as blog_views
+from blog import urls as blog_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('blog/', blog_urls)
     path('', blog_views.index)
 ]
